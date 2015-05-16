6c511720d06d2805242d6194e024987cfa5d9381
Sakamoto <yskibouyskibou@gmail.com>
2015-05-16 23:12:23 +0900

    complete

--- ros.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/ros/indigo/setup.bash
-source ~/catkin_ws/devel/setup.bash
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ bool permit_nothing()
   cout <<"OK!  What do you want to permit?" << endl;
   cout <<"1:JOYSTICK(js*)" << endl;
   cout <<"2:ARDUINO(ttyACM*)" << endl;
-  cout <<"3:iMCs01(urbtc*)->4,5,6,3" << endl;
+  cout <<"3:iMCs01(urbtc*)" << endl;
+  cout <<"4:iMCs01(install module)" << endl;
   cout <<"Please input your command.Like \"./main 1\"" << endl;
 }
 
